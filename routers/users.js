425aecd2425ccffa5fc0a3b1bff9834bbf9fbed2
HADI AMAN <hadiaman2.0@gmail.com>
2021-09-08 21:28:16 +0530

    To prevent Cross-Site Request Forgery Attack aka CSRF Attack while creating a new user, a csrf token provided while calling a GET request to user end point.

--- routers/users.js
+++ routers/users.js
@@ -1,39 +1,66 @@
-const { validate, User } = require('../models/users');
+const {
+    validate,
+    User
+} = require('../models/users');
 const router = require('express').Router();
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
-const { auth, validateAuthDelete } = require('../middleware/auth');
+const {
+    auth,
+    validateAuthDelete
+} = require('../middleware/auth');
+const {
+    v4
+} = require('uuid');
 
-router.post('/', async(req, res)=>{
-    
-    const {error} = validate(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+let csrfToken;
 
-    let user = await User.findOne({email: req.body.email});
-    if(user) return res.status(400).send('Email is already in use.');
+router.get('/', (req, res)=>{
+    csrfToken = v4();
+    console.log(csrfToken);
+    res.header('x-csrf-token', csrfToken).send('CSRF_');
+});
+
+router.post('/', async (req, res) => {
+    if (!req.header('x-csrf-token') || (csrfToken !== req.header('x-csrf-token')))
+        return res.status(400).send('Invalid CSRF token.')
+
+    const {
+        error
+    } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({
+        email: req.body.email
+    });
+    if (user) return res.status(400).send('Email is already in use.');
 
     user = new User(_.pick(req.body, ['name', 'email', 'password']));
     user.password = await bcrypt.hash(user.password, await bcrypt.genSalt(10));
     await user.save();
 
     const token = user.generateAuthenticationToken();
-
     res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
-router.delete('/', auth, async(req, res)=>{
+router.delete('/', auth, async (req, res) => {
+    const {
+        error
+    } = validateAuthDelete(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-    const {error} = validateAuthDelete(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    const user = await User.findOne({_id: req.user._id});
-    if(!user) return res.status(400).send('Invalid credentials.');
+    const user = await User.findOne({
+        _id: req.user._id
+    });
+    if (!user) return res.status(400).send('Invalid credentials.');
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if(!validPassword) return res.status(400).send('Invalid credentials..')
+    if (!validPassword) return res.status(400).send('Invalid credentials..')
 
-    await User.findOneAndDelete({_id: req.user._id});
-    
+    await User.findOneAndDelete({
+        _id: req.user._id
+    });
+
     res.send(user);
 })
 
